8662ebff34ba4d6b675e98488a4b6162073ee5c3
florianbsc <floflo_200068@hotmail.fr>
2024-10-16 22:27:37 +0200

    debut de la logique de changement de form

---
+++ casseBrique-vue/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import brique from './components/brique.vue';
+  import { ref } from 'vue';
+
+  const tableau = ref([0,0,0,0,0,0,0,0,0,0])
+  const etatBrique = (place) => {
+    console.log(place)
+    tableau.value[place]++
+ }
+
+</script>
+
+
+
+
+<template>
+
+  <div v-for="brique, index in 10">
+    <brique @click="etatBrique(index)" :state="tableau[index]"/>
+  </div>
+</template>
+
+
+
+
+<style scoped>
+
+</style>
---
+++ casseBrique-vue/src/components/brique.vue
@@ -0,0 +1,54 @@
+
+<script setup>
+    const props = defineProps({
+        state: {
+            type: Number, default: 0,
+        },
+        
+    })
+
+
+</script>
+
+
+<template>
+    <div v-if="props.state <= 2 " >
+        <div class="Brique" v-if="props.state == 0" >
+             {{ props.place}}
+        </div>
+        <div class="Triangle" v-else-if="props.state == 1">
+
+        </div>
+        <div class="Cercle" v-else-if="props.state == 2">
+
+        </div>
+    </div>
+    
+</template>
+
+
+<!-- scoped = css que pour ce fichier -->
+ <!-- "." pour les class et "#" pour les Id -->
+<style lang="scss" scoped>
+    .Brique {
+        width: 60px ;
+        height: 60px;
+        background-color: cyan;
+    }
+    .Triangle{
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 0 30px 60px 30px;
+        border-color: transparent transparent green transparent;
+
+
+    }
+    .Cercle {
+        width: 60px ;
+        height: 60px;
+        background-color: magenta;
+        border-radius: 50%;
+    }
+
+</style>
